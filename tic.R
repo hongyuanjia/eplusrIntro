2d4400239db25b5887d4450ed08276c8113e6559
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-05-04 17:04:53 +0800

    Install EnergyPlus when compling slides.

--- tic.R
+++ tic.R
@@ -6,6 +6,7 @@ if (Sys.getenv("id_rsa") != "") {
         add_step(step_setup_ssh())
 
     get_stage("deploy") %>%
+        add_code_step(eplusr::install_eplus(8.8)) %>%
         add_code_step(knitr::knit("index.Rmd")) %>%
         add_step(step_push_deploy(path = "branch", branch = "gh-pages"))
 }
