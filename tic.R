ab9b85d3e59078cd6d4e8af8e6921bdf3c722747
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-05-04 23:56:38 +0800

    Update tic.R

--- tic.R
+++ tic.R
@@ -1,16 +1,17 @@
 get_stage("before_install") %>%
     add_code_step(update.packages(ask = FALSE))
 
-get_stage("install") %>%
-     step_install_github(c("gadenbuie/xaringanthemer", "ropenscilabs/icon"))
-
-
 if (Sys.getenv("id_rsa") != "") {
     get_stage("before_deploy") %>%
-        add_step(step_setup_ssh())
+        add_step(step_setup_ssh()) %>%
+        add_step(step_setup_push_deploy(path = ".", branch = "gh-pages", remote_url = NULL,
+            orphan = TRUE, checkout = FALSE))
 
     get_stage("deploy") %>%
         add_code_step(eplusr::install_eplus(8.8)) %>%
-        add_code_step(knitr::knit("index.Rmd")) %>%
-        add_step(step_push_deploy(path = "branch", branch = "gh-pages"))
+        add_code_step(remotes::install_github("gadenbuie/xaringanthemer")) %>%
+        add_code_step(remotes::install_github("ropenscilabs/icon")) %>%
+        add_code_step(install.packages("xaringan")) %>%
+        add_code_step(rmarkdown::render("index.Rmd")) %>%
+        add_step(step_push_deploy(path = ".", branch = "gh-pages"))
 }
