eba0ddcaf8b273761c90af6c13f10638aea2bcf1
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-05-04 16:56:21 +0800

    Update CI.

---
+++ tic.R
@@ -0,0 +1,11 @@
+get_stage("before_install") %>%
+    add_code_step(update.packages(ask = FALSE))
+
+if (Sys.getenv("id_rsa") != "") {
+    get_stage("before_deploy") %>%
+        add_step(step_setup_ssh())
+
+    get_stage("deploy") %>%
+        add_code_step(knitr::knit("index.Rmd")) %>%
+        add_step(step_push_deploy(path = "branch", branch = "gh-pages"))
+}
