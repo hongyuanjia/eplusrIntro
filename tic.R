4e04e27f5c749a6b506fbc678f138de422411f3a
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-05-04 23:39:06 +0800

    Update tic.R

--- tic.R
+++ tic.R
@@ -1,10 +1,11 @@
 get_stage("before_install") %>%
     add_code_step(update.packages(ask = FALSE))
 
-if (Sys.getenv("id_rsa") != "") {
-    get_stage("install") %>%
-        step_install_github(c("gadenbuie/xaringanthemer", "ropenscilabs/icon"))
+get_stage("install") %>%
+     step_install_github(c("gadenbuie/xaringanthemer", "ropenscilabs/icon"))
 
+
+if (Sys.getenv("id_rsa") != "") {
     get_stage("before_deploy") %>%
         add_step(step_setup_ssh())
 
