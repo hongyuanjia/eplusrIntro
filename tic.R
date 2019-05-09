38fd198ecd0ca7c8f21b1ae3c61fee3cf7fe4dcd
Hongyuan Jia <hongyuanjia@cqu.edu.cn>
2019-05-09 11:59:36 +0800

    Use `remotes::install_deps()` to install required pkgs.

--- tic.R
+++ tic.R
@@ -1,6 +1,3 @@
-get_stage("before_install") %>%
-    add_code_step(update.packages(ask = FALSE))
-
 if (Sys.getenv("id_rsa") != "") {
     get_stage("before_deploy") %>%
         add_step(step_setup_ssh()) %>%
@@ -8,10 +5,7 @@ if (Sys.getenv("id_rsa") != "") {
             orphan = TRUE, checkout = FALSE))
 
     get_stage("deploy") %>%
-        add_code_step(eplusr::install_eplus(8.8)) %>%
-        add_code_step(remotes::install_github("gadenbuie/xaringanthemer")) %>%
-        add_code_step(remotes::install_github("ropenscilabs/icon")) %>%
-        add_code_step(install.packages("xaringan")) %>%
+        add_code_step(remotes::install_deps()) %>%
         add_code_step(rmarkdown::render("index.Rmd")) %>%
         add_step(step_push_deploy(path = ".", branch = "gh-pages"))
 }
